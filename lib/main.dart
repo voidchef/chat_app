780200dd514ecb78391414292ae356bab600a5a4
voidchef <neelutpal.saha@s.amity.edu>
2023-11-19 19:52:55 +0530

    feat: add auth screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:chat_app/screens/auth.dart';
+
 void main() {
   runApp(const App());
 }
@@ -10,16 +12,13 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'FlutterChat',
-        theme: ThemeData().copyWith(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: const Color.fromARGB(255, 63, 17, 177)),
-        ),
-        home: const Scaffold(
-          body: Center(
-            child: Text('Lets Begin!'),
-          ),
-        ));
+      title: 'FlutterChat',
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 63, 17, 177)),
+      ),
+      home: const AuthScreen(),
+    );
   }
 }
